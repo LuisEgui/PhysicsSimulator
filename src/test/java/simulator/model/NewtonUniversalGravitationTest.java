c997ad525996960abb3de376e94aa375e71ba616
Luis Egui <misionpage@gmail.com>
2021-03-15 21:49:21 +0100

    * Sonar corrections

--- src/test/java/simulator/model/NewtonUniversalGravitationTest.java
+++ src/test/java/simulator/model/NewtonUniversalGravitationTest.java
@@ -21,7 +21,7 @@ class NewtonUniversalGravitationTest {
             b1 = new Body.Builder().id("b1").velocity(new Vector2D())
                     .position(new Vector2D()).mass(10).build();
             b2 = new Body.Builder().id("b2").velocity(new Vector2D())
-                    .position(new Vector2D(0,1)).mass(5).build();
+                    .position(new Vector2D(0,2)).mass(5).build();
             nug = new NewtonUniversalGravitation();
             bodies = new ArrayList<>();
             bodies.add(b1); bodies.add(b2);
@@ -29,7 +29,9 @@ class NewtonUniversalGravitationTest {
 
     @Test
     void testApply() {
-        Vector2D expectedForceOverBodyOne = new Vector2D(0,6.67E-11*b1.getMass()*b2.getMass());
+        Vector2D r = b2.getPosition().minus(b1.getPosition());
+        double rSquare = Math.pow(r.magnitude(), 2);
+        Vector2D expectedForceOverBodyOne = r.unitVector().scale((6.67E-11 * b1.getMass() * b2.getMass())/rSquare);
         nug.apply(bodies);
         assertEquals(expectedForceOverBodyOne, bodies.get(0).getForce());
         assertEquals(expectedForceOverBodyOne.scale(-1), bodies.get(1).getForce());
