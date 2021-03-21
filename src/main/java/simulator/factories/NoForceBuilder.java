772354605c210ef556b0c6cbe0d511ea001bf1ee
Luis Egui <misionpage@gmail.com>
2021-03-21 04:08:13 +0100

    * Se ha implementado la clase NoForceBuilder. * Cambios minimos en BasicBodyBuilder. * Se ha implementado el test unitario NoForceBuilderTest. * TODO: falta testear el método "createData() : JSONObject"

---
+++ src/main/java/simulator/factories/NoForceBuilder.java
@@ -0,0 +1,30 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+import simulator.model.forcelaws.NoForce;
+
+import java.util.Objects;
+
+public class NoForceBuilder extends Builder<NoForce> {
+
+    private static final String DESCRIPTION = "A force law that doesn't apply any aditional force";
+
+    @Override
+    public NoForce createTheInstance(JSONObject data) {
+        Objects.requireNonNull(data);
+        NoForce noForce;
+        if(super.type == TypeTag.NF) {
+            noForce = new NoForce();
+            return noForce;
+        } else
+            throw new IllegalArgumentException("Typetag doesn't match with the builder constructor!");
+    }
+
+    @Override
+    public JSONObject createData() {
+        JSONObject template = new JSONObject();
+        template.put("type", super.type.toString().toLowerCase());
+        template.put("desc", DESCRIPTION);
+        return template;
+    }
+}
---
+++ src/test/java/simulator/factories/NoForceBuilderTest.java
@@ -0,0 +1,40 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import simulator.model.forcelaws.NoForce;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoForceBuilderTest {
+    NoForceBuilder noForceBuilder;
+    NoForce noForce;
+    JSONObject info = new JSONObject();
+
+    @BeforeEach
+    void before() {
+        info.put("type", "nf");
+        noForceBuilder = new NoForceBuilder();
+        noForce = new NoForce();
+    }
+
+    @Test
+    void testCreateInstance() {
+        info.put("data", new JSONObject());
+        NoForce expectedNoForce = noForceBuilder.createInstance(info);
+        assertNotEquals(null, expectedNoForce);
+    }
+
+    @Test
+    void testInvalidCreateInstance() {
+        info.put("type", "nlug");
+        info.put("data", new JSONObject());
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                this::executeInvalidNoForce);
+        assertEquals("Typetag doesn't match with the builder constructor!", exception.getMessage());
+    }
+
+    private void executeInvalidNoForce() {
+        noForceBuilder.createTheInstance(info);
+    }
+}
