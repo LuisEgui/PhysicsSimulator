ecdb6538680998ce4d7c6b946efad4422b1174b2
Luis Egui <misionpage@gmail.com>
2021-04-01 16:42:11 +0200

    * Cambios en la estructura del StateComparator. Ahora la clase EqualStates implementa dicha interfaz, y las clases MassEqualStates y EpsilonEqualStates heredan de esta.

--- src/main/java/simulator/control/MassEqualStates.java
+++ src/main/java/simulator/control/MassEqualStates.java
@@ -1,43 +1,19 @@
 package simulator.control;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import simulator.factories.*;
 import simulator.model.bodies.FluentBuilder.Body;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class MassEqualStates implements StateComparator {
+public class MassEqualStates extends EqualStates {
 
     public MassEqualStates() {
         // Nothing to construct.
     }
 
-    private static boolean equalBodyList(List<Body> bodyListOne, List<Body> bodyListTwo) {
+    public boolean equalBodyList(List<Body> bodyListOne, List<Body> bodyListTwo) {
         List<Body> bodyList = bodyListOne.stream().filter(body2 -> bodyListTwo.stream()
                 .anyMatch(body1 -> body1.getId().equals(body2.getId()) && body1.getMass() == body2.getMass()))
                 .collect(Collectors.toList());
         return bodyListOne.size() == bodyList.size() && bodyListTwo.size() == bodyList.size();
-    }
-
-    @Override
-    public boolean compare(JSONObject s1, JSONObject s2) {
-        Objects.requireNonNull(s1); Objects.requireNonNull(s2);
-        JSONArray s1JSONArray;
-        JSONArray s2JSONArray;
-        List<Body> s1Bodies;
-        List<Body> s2Bodies;
-        BasicBodyBuilder bodyBuilder = new BasicBodyBuilder();
-
-        if(s1.getDouble("time") != s2.getDouble("time"))
-            return false;
-
-        s1JSONArray = s1.getJSONArray("bodies");
-        s2JSONArray = s2.getJSONArray("bodies");
-        s1Bodies = bodyBuilder.getBodiesFromJSONArray(s1JSONArray);
-        s2Bodies = bodyBuilder.getBodiesFromJSONArray(s2JSONArray);
-        return equalBodyList(s1Bodies, s2Bodies);
     }
 }
