fb135cd83331a62b0004b30943c1bba22b76c181
Luis Egui <misionpage@gmail.com>
2021-04-01 16:43:01 +0200

    * Se ha implementado el builder de MassEqualStates: MassEqualStateBuilder. * Se ha implementado el test unitario MassEqualStateBuilderTest.

---
+++ src/test/java/simulator/factories/MassEqualStateBuilderTest.java
@@ -0,0 +1,46 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import simulator.control.MassEqualStates;
+
+class MassEqualStateBuilderTest {
+    MassEqualStateBuilder massEqualStateBuilder;
+    MassEqualStates massEqualStates;
+    JSONObject info = new JSONObject();
+
+    @BeforeEach
+    void before() {
+        info.put("type", "masseq");
+        massEqualStateBuilder = new MassEqualStateBuilder();
+        massEqualStates = new MassEqualStates();
+    }
+
+    @Test
+    void testCreateInstance() {
+        info.put("data", new JSONObject());
+        MassEqualStates expectedStateComparator = massEqualStateBuilder.createInstance(info);
+        assertNotEquals(null, expectedStateComparator);
+    }
+
+    @Test
+    void testGetDescription() {
+        String expectedDescription = "A mass equal state comparator";
+        assertEquals(expectedDescription, massEqualStateBuilder.getDescription());
+    }
+
+    @Test
+    void testInvalidCreateInstance() {
+        info.put("type", "nlug");
+        info.put("data", new JSONObject());
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                this::executeInvalidStateComparator);
+        assertEquals("Typetag doesn't match with the builder constructor!", exception.getMessage());
+    }
+
+    private void executeInvalidStateComparator() {
+        massEqualStateBuilder.createTheInstance(info);
+    }
+}
